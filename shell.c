35d85b00c2aa4cbd77a6a841d467dff84cd34186
Becca615 <damilolarebecca8@gmail.com>
2021-11-03 06:42:11 -0700

    shell

--- shell.c
+++ shell.c
@@ -1,49 +1,39 @@
-#include "main.h"
-/**
- * main - basic shell emulation.
- * @ac: argument counter.
- * @av: argument vector.
- * Return: 0 on succes.
- */
-int main(int ac, char **av)
-{
-	int bytes_read, status, cuenta = 0;
-	size_t size = 0;
-	char *text = 0, *prompt = "$ ", *argv[] = {NULL, NULL, NULL, NULL};
-	pid_t newpid;
-	struct stat fStat;
-	(void)ac;
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, prompt, 2);
-	while ((bytes_read = getline(&text, &size, stdin)))
+int main(void)
+{
+	int bytes_read;
+	size_t size;
+	char *text;
+	char *argv[] = {NULL, NULL};
+
+	while (1)
 	{
-		if (bytes_read == EOF)
-			free_EOF(text);
-		if (text == NULL)
-			free_textNULL(text);
-		cuenta++;
-		create_argv(text, argv);
-		newpid = fork();
-		if (newpid == -1)
-			freeNewpid();
-		if (newpid == 0)
+		printf("#cisfun$ ");
+		text = (char *) malloc(size);
+		bytes_read = getline(&text, &size, stdin);
+		if (bytes_read == -1)
 		{
-			if (stat(argv[0], &fStat) == 0)
-			{
-				execve(argv[0], argv, NULL);
-				free(text);
-			}
-			else
-				print_terror(argv, av[0], cuenta, text);
+			puts("ERROR!");
 		}
-		else
-			wait(&status);
-		bytes_read = 0;
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, prompt, 2);
+		if (text[bytes_read - 1] == '\n')
+		{
+			text[bytes_read - 1] = 0;
+		}
+		if (fork() == 0)
+		{
+			argv[0] = text;
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("Error:");
+			}
+			exit(0);
+		}
+		wait(NULL);
 	}
-	if (bytes_read == -1)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
+	return(0);
 }
