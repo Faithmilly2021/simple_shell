09dace74fce98b93be13cb5c5eb274ee91e417fc
Becca615 <damilolarebecca8@gmail.com>
2021-11-05 10:08:27 -0700

    update

--- exec.c
+++ exec.c
@@ -1,88 +1,20 @@
-#include "shell.h"
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * handle_builtin - Handle Builtin Command
- * @cmd: Parsed Command
- * @er:statue of last Excute
- * Return: -1 Fail 0 Succes (Return :Excute Builtin)
- */
-
-int handle_builtin(char **cmd, int er)
-{
-	 bul_t bil[] = {
-		{"cd", change_dir},
-		{"env", dis_env},
-		{"help", display_help},
-		{"echo", echo_bul},
-		{"history", history_dis},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while ((bil + i)->command)
-	{
-		if (_strcmp(cmd[0], (bil + i)->command) == 0)
-		{
-			return ((bil + i)->fun(cmd, er));
-		}
-		i++;
-	}
-	return (-1);
-}
-/**
- * check_cmd - Excute Simple Shell Command (Fork,Wait,Excute)
+ * main - execve example
  *
- * @cmd:Parsed Command
- * @input: User Input
- * @c:Shell Excution Time Case of Command Not Found
- * @argv:Program Name
- * Return: 1 Case Command Null -1 Wrong Command 0 Command Excuted
+ * Return: Always 0.
  */
-int check_cmd(char **cmd, char *input, int c, char **argv)
+int main(void)
 {
-	int status;
-	pid_t pid;
+	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
 
-	if (*cmd == NULL)
+	printf("Before execve\n");
+	if (execve(argv[0], argv, NULL) == -1)
 	{
-		return (-1);
+		perror("Error:");
 	}
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Error");
-		return (-1);
-	}
-
-	if (pid == 0)
-	{
-		if (_strncmp(*cmd, "./", 2) != 0 && _strncmp(*cmd, "/", 1) != 0)
-		{
-			path_cmd(cmd);
-		}
-
-		if (execve(*cmd, cmd, environ) == -1)
-		{
-			print_error(cmd[0], c, argv);
-			free(input);
-			free(cmd);
-			exit(EXIT_FAILURE);
-		}
-		return (EXIT_SUCCESS);
-	}
-	wait(&status);
+	printf("After execve\n");
 	return (0);
-}
-/**
- * signal_to_handel - Handle ^C
- * @sig:Captured Signal
- * Return: Void
- */
-void signal_to_handel(int sig)
-{
-	if (sig == SIGINT)
-	{
-		PRINTER("\n$ ");
-	}
 }
